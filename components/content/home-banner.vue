2cf2b7da7030a01a293eb7d3f3b1f3b62792f4ab
Maelle05 <maelle.rabouan@gmail.com>
2022-09-12 18:22:51 +0200

    wip(): annim text

--- components/content/home-banner.vue
+++ components/content/home-banner.vue
@@ -3,8 +3,39 @@
     <slot name="bg-image" />
     <div class="home-banner__filtre"></div>
     <slot />
+    <div class="home-banner__annim-text">
+      <p>Traveaux sur mesure <span> Dressing </span></p>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    mounted: function() {
+      this.step = 0
+      this.values = ['Dressing' , 'Cuisine', 'Placards', 'Mobilier d’agencement', 'Parquet', 'Bains', 'Portes d’entrée', 'Fênetres', 'Terrasse', 'Pergolas' , 'Mobilier d’exterieur', 'Portail', 'Clôture' ]
+      this.span = document.querySelector('.home-banner__annim-text span')
+      this.annimText(this.step)
+    },
+    methods: {
+      submitMessage() {
+        console.log(this.socket) // something
+      },
+      annimText(i) {
+        this.span.innerHTML = this.values[i];
+        setTimeout(()=>{
+          if (i < this.values.length-1) {
+            this.step++
+            this.annimText(this.step)
+          } else {
+            this.step = 0
+            this.annimText(this.step)
+          }
+        }, 1300)
+      }
+    }
+  }
+</script>
 
 <style>
 .home-banner{
@@ -35,7 +66,51 @@
   font-family: 'Roboto', sans-serif;
   font-weight: 500;
   font-size: 80px;
-  left: calc(50% - 345px);
-  margin: 140px 0 0 0;
+  width: 100%;
+  margin: 140px auto 0 auto;
+  text-align: center;
 }
+
+.home-banner__annim-text{
+  color: #FFFFFF;
+  position: absolute;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 36px;
+  width: 100%;
+  margin: 244px auto 0 auto;
+  text-align: center;
+  color: #000002;
+}
+
+.home-banner__annim-text span {
+  color: #951C3C;
+}
+
+@media only screen and (max-width : 900px){
+  .home-banner h1{
+    font-size: 60px;
+  }
+
+  .home-banner__annim-text{
+    margin: 218px auto 0 auto;
+    font-size: 30px;
+  }
+}
+
+@media only screen and (max-width : 517px){
+  .home-banner__annim-text{
+    margin: 296px auto 0 auto;
+    font-size: 26px;
+  }
+}
+
+
+@media only screen and (max-width : 312px){
+  .home-banner h1{
+    font-size: 40px;
+  }
+  
+}
+
 </style>
