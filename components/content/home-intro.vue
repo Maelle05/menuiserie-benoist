f2ba9838b3b47700d9e0d34f1c5afcf0d1ad0344
Maëlle Rabouan <56165289+Maelle05@users.noreply.github.com>
2022-09-13 16:37:11 +0200

    Create home-intro.vue

---
+++ components/content/home-intro.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="home-intro">
+        <div class="home-intro__images"><slot name="images" /></div>
+        <div class="home-intro__text"><slot /></div>
+    </div>
+</template>
+
+<style>
+    .home-intro{
+        width: 85%;
+        margin: auto;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 30px;
+        margin-top: 100px;
+    }
+
+    .home-intro__images{
+        grid-column: 1 / 2;
+    }
+
+    .home-intro__images p img{
+        max-width: 80%;
+    }
+
+    .home-intro__text{
+        grid-column: 2 / 2;
+    }
+
+    .home-intro__text p {
+        margin-top: 10px;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 300;
+        font-size: 24px;
+        line-height: 32px;
+    }
+
+    .home-intro__text h2{
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 50px;
+        line-height: 59px;
+        margin-top: 0;
+    }
+
+    .home-intro__text h2 a {
+        text-decoration: none;
+        color: black;
+    }
+
+    @media only screen and (max-width : 900px){
+        .home-intro__images{
+            grid-column: 1 / 3;
+            grid-row: 1;
+        }
+
+        .home-intro__text{
+            grid-column: 1 / 3;
+            grid-row: 2;
+        }
+
+    }
+</style>
