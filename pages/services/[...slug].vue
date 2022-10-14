a2ea359942a45b02fe66c3b854a8a0da81964dc7
Maelle05 <maelle.rabouan@gmail.com>
2022-10-14 11:35:20 +0200

    feat(): loader imgs services

--- pages/services/[...slug].vue
+++ pages/services/[...slug].vue
@@ -10,6 +10,9 @@
         </div>
       </template>
     </ContentDoc>
+    <div class="container-loader" ref="loader">
+      <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    </div>
     <div class="container-masonry" ref="masonry">
     </div>
   </div>
@@ -34,6 +37,7 @@ export default{
       this.$refs.masonry.appendChild(container)
 
       myImg.addEventListener('error', () =>{
+        this.$refs.loader.style.display = 'none';
         myImg.style.display = 'none';
         new MiniMasonry({
           container: this.$refs.masonry,
@@ -116,5 +120,68 @@ p{
   font-size: 20px;
   text-align: center;
   margin-bottom: 16px;
+}
+
+.container-loader{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 33px;
+  width: 13px;
+  height: 13px;
+  border-radius: 50%;
+  background: #ab1e43;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 8px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 8px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 32px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 56px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(24px, 0);
+  }
 }
 </style>
